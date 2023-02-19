4cde063c30637d697603ae60778d8ccc0161c33d
chetan B <125805035+santhu3004@users.noreply.github.com>
2023-02-19 22:02:11 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, arunkumar\n";
+  cout << "Hello, chetan\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
   return 0;
